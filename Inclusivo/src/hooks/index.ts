0d9f200ea783fdd89bf53b8a51eaf30e0379db1c
Fernando Medina Vislao <152422080+Fernando-Alvaro-Medina-Vislao@users.noreply.github.com>
2025-11-10 21:13:26 -0500

    Delete Inclusivo directory

--- Inclusivo/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all hooks for easy importing
-export * from './useSchedule';
-export * from './useNotifications';
-export * from './useNavigation';
-export * from './useDocuments';
-export * from './useMobileGestures';
--- Inclusivo/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all models for easy importing
-export * from './User';
-export * from './Schedule';
-export * from './Notification';
-export * from './Document';
-export * from './Navigation';
