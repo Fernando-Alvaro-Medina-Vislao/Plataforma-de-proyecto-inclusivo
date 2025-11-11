b259dec09f6821ca8c824462e3cea467271dcaa2
Fernando Medina Vislao <152422080+Fernando-Alvaro-Medina-Vislao@users.noreply.github.com>
2025-11-10 21:04:37 -0500

    Delete AccessiLearn UI_UX Design (1) directory

--- AccessiLearn UI_UX Design (1)/src/services/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all services for easy importing
-export * from './ScheduleService';
-export * from './NotificationService';
-export * from './NavigationService';
-export * from './DocumentService';
